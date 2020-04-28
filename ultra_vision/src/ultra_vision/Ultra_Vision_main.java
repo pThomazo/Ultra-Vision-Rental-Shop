ec02c7527b9e16f6df32ab47df8937c3fd2b4687
jozimar basilio <jozimarbasiliof@gmail.com>
2020-04-28 23:29:52 +0100

    Test

--- ultra_vision/src/ultra_vision/Ultra_Vision_main.java
+++ ultra_vision/src/ultra_vision/Ultra_Vision_main.java
@@ -1,6 +1,7 @@
 package ultra_vision;
 
-public class Ultra_Vision_main {
+public class Ultra_Vision_main extends JFrame {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
