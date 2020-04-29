d44d9d8de16347190c1e5f380e017c4f6a3aa76d
jozimar basilio <jozimarbasiliof@gmail.com>
2020-04-29 12:29:05 +0100

    main created

--- src/ultra_vision/Ultra_Vision_main.java
+++ src/ultra_vision/Ultra_Vision_main.java
@@ -4,6 +4,11 @@ import javax.swing.JFrame;
 
 public class Ultra_Vision_main extends JFrame {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
