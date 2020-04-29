b0b29f73d0b2cc46286bc72015e2780e191720ed
pThomazo <priscila_thomazo@outlook.com>
2020-04-29 16:41:19 +0100

    writing the customer class

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,35 @@
 package model;
 
 public class Customer {
+	
+	private int id;
+	private String name;
+	private MembershipCard memberCard;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public MembershipCard getMemberCard() {
+		return memberCard;
+	}
+	
+	public void setMemberCard(MembershipCard memberCard) {
+		this.memberCard = memberCard;
+	}
+	
+	
 
 }
