95a87135161209c1f20b0e0c2c5e890eecf5f3cc
jozimar basilio <jozimarbasiliof@gmail.com>
2020-04-30 14:03:08 +0100

    Customer Table arraylist<> created.

---
+++ src/ultra_vision/CustomersTableModel.java
@@ -0,0 +1,62 @@
+package ultra_vision;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import model.Customer;
+
+public class CustomersTableModel extends AbstractTableModel {
+	
+	
+	 
+	private static final long serialVersionUID = 1L;
+	private List<Customer> customers;
+	private static final String[] columnNames = new String[] {"ID","Name"};
+	
+	  //creating the arrayList customer
+	public CustomersTableModel() {
+		customers = new ArrayList<Customer>();
+	}
+	
+// the column name is going to be returned 
+	@Override
+	public String getColumnName(int column) {
+		return columnNames[column];
+	}
+//get the row where name is stored 
+	@Override
+	public int getRowCount() {
+	return customers.size() ;
+	}
+	
+	
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+//here  return the element at he specified index
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+	Customer titleAtRow = customers.get(rowIndex);
+	
+	switch(columnIndex) {
+	case 0:
+		return titleAtRow.getId();
+		
+	case 1:
+		return titleAtRow.getName();
+		
+		default: 
+			return null;
+	}
+	}
+	
+	public void setCustomers(List<Customer> customers) {
+		this.customers = customers;
+		this.fireTableDataChanged();
+		
+		
+	}
+
+}
