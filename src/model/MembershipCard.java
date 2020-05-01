03ced9f383802c8076f6cae0882980f72f76a845
pThomazo <priscila_thomazo@outlook.com>
2020-05-01 01:05:10 +0100

    writing the Membership class

--- src/model/MembershipCard.java
+++ src/model/MembershipCard.java
@@ -1,5 +1,47 @@
 package model;
 
 public class MembershipCard {
+	
+	private int id;
+	private int limite;
+	private String cardNumber;
+	private Plan plan;
+	
+	public MembershipCard() {
+		setLimite(4);
+		
+	}
+	
+	public Plan getPlan() {
+		return plan;
+	}
+	
+	public void setPlan(Plan plan) {
+		this.plan = plan;
+	}
+	
+	public int getLimit() {
+		return limite;
+	}
+	
+	public void setLimite(int limite) {
+		this.limite = limite;
+	}
+	
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
 
 }
