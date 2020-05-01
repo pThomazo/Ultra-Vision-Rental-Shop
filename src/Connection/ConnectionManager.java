cb5b59072b658d6bd56286f9c5eae94e93b1371e
pThomazo <priscila_thomazo@outlook.com>
2020-05-01 01:23:27 +0100

    writing the connector for the DB on AWS

--- src/Connection/ConnectionManager.java
+++ src/Connection/ConnectionManager.java
@@ -1,5 +1,36 @@
 package Connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionManager {
+	
+	//we may change the DB
+	public static final String URL = "jdbc:mysql://database-1.ckpglua8ft7e.eu-west-1.rds.amazonaws.com:3306/ultra_vision_rs?useTimezone=true&severTimezone=UTC";
+	private Connection con;
+	
+	//getting  connection from the database on AWS
+	public Connection getConnection() throws SQLException{
+		if(con==null) {
+			Connection con = null;
+			
+			try {
+				con = DriverManager.getConnection(URL,"admin", "CCT20201234!");
+				//con = DriverManager.getConnection(URL_Local,"root", "");
+			}
+			catch(SQLException e) {
+				e.printStackTrace();
+				
+				//Here the program should fail
+				
+				throw e;
+			}
+			this.con = con;
+		}
+		
+		//returning the connection status
+		return con;
+	}
 
 }
