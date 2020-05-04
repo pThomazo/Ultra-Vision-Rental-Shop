6553b61e66a58573647792acf3b1185a55222b8f
pThomazo <priscila_thomazo@outlook.com>
2020-05-04 15:39:27 +0100

    enum Plan

--- src/model/Plan.java
+++ src/model/Plan.java
@@ -1,5 +1,16 @@
 package model;
 
-public class Plan {
-
+public enum Plan {
+	
+	
+	
+	MusicLovers(cod: 0, name: "Music Lovers"), VideoLovers(cod: 1, name: "Video Lovers"), TVLovers(cod: 2, name: "TV Lovers"), Premium(cod: 3, name: "Preminum");
+	
+	public int cod;
+	public String name;
+	
+	private Plan (int cod, String name) {
+		this.cod = cod;
+		this.name = name;
+	}
 }
