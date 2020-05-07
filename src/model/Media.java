887329a01c409f65a519b01ea7377a979b6920a6
jozimar basilio <jozimarbasiliof@gmail.com>
2020-05-07 21:59:43 +0100

    desfiz

--- src/model/Media.java
+++ src/model/Media.java
@@ -2,39 +2,6 @@ package model;
 
 public enum Media {
 	
-	CD( 0,"CD"), DVD( 1, "DVD"), BLURAY( 2,"Blueray");
 	
-	int cod;
-	String name;
-	
-	private Media(int cod, String name) {
-		this.cod = cod;
-		this.name = name;
-	}
-	
-	public int getCod() {
-		return cod;
-	}
-	public void setCod(int cod) {
-		this.cod = cod;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public static Media fromCode(int code) {
-		switch (code) {
-		
-		case 0: return Media.CD;
-		case 1: return Media.DVD;
-		case 2: return Media.BLURAY;
-		
-		default: throw new IllegalArgumentException();
-		}
-	}
 
 }
--- src/model/Titletype.java
+++ src/model/Titletype.java
@@ -2,28 +2,5 @@ package model;
 
 public enum Titletype {
 
-	Music(0, "Music"), LiveConcert(1, "Live concert"), Movie(2, "Movie"), BoxSet(3, "Box set");
-
-	public int cod;
-	public String name;
-
-	private Titletype(int cod, String name) {
-		this.cod = cod;
-		this.name = name;
-	}
-
-	public static Titletype fromCode(int code) {
-		switch (code) {
-		case 0:
-			return Music;
-		case 1:
-			return Titletype.LiveConcert;
-		case 2:
-			return Movie;
-		case 3:
-			return BoxSet;
-		default:
-			throw new IllegalArgumentException();
-		}
-	}
+	
 }
