3a381d48d84aeb669d11fcc027c49eeae75ff490
jozimar basilio <jozimarbasiliof@gmail.com>
2020-05-09 09:48:11 +0100

    Update

--- src/ultra_vision/ManageCustomers.java
+++ src/ultra_vision/ManageCustomers.java
@@ -19,13 +19,14 @@ public ManageCustomers(Ultra_Vision_main mf) {
 	setBounds(0, 0, 455, 303);
 	setLocationByPlatform(true);
 	
+	//setting the results in order column and rows 
 	GridBagLayout gridBagLayout = new GridBagLayout();
 	gridBagLayout.columnWidths = new int[] {0,0,0,0};
 	gridBagLayout.rowHeights = new int[] {0,0};
 	gridBagLayout.columnWeights = new double[] {0.0, 0.0, 0.0, Double.MIN_VALUE};
 	gridBagLayout.rowWeights = new double[] {0.0, Double.MIN_VALUE};
 	getContentPane().setLayout(gridBagLayout);
-	
+	//creating the new customer's button
 	JButton button = new JButton("New customer");
 	button.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
@@ -33,7 +34,7 @@ public ManageCustomers(Ultra_Vision_main mf) {
 			dNC.setVisible(true);
 		}
 	});
-	
+	//setting button's position and colour
 	button.setBorder(new LineBorder(new Color(255, 228, 196),2 , true));
 	button.setBackground(new Color(255, 228, 196));
 	GridBagConstraints gbc_button = new GridBagConstraints();
@@ -45,6 +46,7 @@ public ManageCustomers(Ultra_Vision_main mf) {
 	gbc_button.gridy=0;
 	getContentPane().add(button, gbc_button);
 	
+	//new button to have the option of update a customers 
 	JButton button_1 = new JButton("Update Customer");
 	button_1.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
@@ -53,6 +55,7 @@ public ManageCustomers(Ultra_Vision_main mf) {
 			dNC.fillCombo();
 		}
 	});
+	////setting button's position,colour
 	button_1.setBorder(new LineBorder(new Color(255, 99, 71), 2, true));
 	button_1.setBackground(new Color(255, 228, 196));
 	GridBagConstraints gbc_button_1 = new GridBagConstraints();
@@ -64,6 +67,7 @@ public ManageCustomers(Ultra_Vision_main mf) {
 	gbc_button_1.gridy = 0;
 	getContentPane().add(button_1, gbc_button_1);
 	
+	////new button to have the option to Find  a customers 
 	JButton button_2 = new JButton("Find customers");
 	button_2.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
